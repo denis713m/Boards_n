27b8900e7897feaea15cde2ceadbf43efac157e0
denis713m <57364006+denis713m@users.noreply.github.com>
2020-11-23 00:16:19 +0200

    Create userReducer

---
+++ frontend/src/redux/reducers/userReducer.js
@@ -0,0 +1,48 @@
+import * as types from '../actionTypes';
+
+const initialState = {
+    isFetching: true,
+    error: false,
+    user: null,    
+  };
+
+  export default function(state = initialState, action) {
+    switch (action.type) {
+      case types.REGISTRATION_USER_REQUEST:{
+        console.log('request');
+        state.isFetching = true;
+        return {...state}
+      }
+      case types.REGISTRATION_USER_SUCCESS:{
+        state.user = action.data;
+        state.isFetching = false;
+        return {...state,
+            isFetching: false,
+            user: action.data,
+            error: null
+        }
+      }
+      case types.REGISTRATION_USER_ERROR:{
+        console.log('error');
+        return {
+          ...state,
+          isFetching: false,
+          error: action.error,
+          user: null
+        }
+      }
+      case types.LOG_OUT:{
+        console.log('log_out');
+        window.localStorage.removeItem('accessToken');
+        return {
+          ...state,
+          isFetching: false,
+          error: true,
+          user: null
+        }
+      }
+      default:
+        return state;
+    }
+  }
+  

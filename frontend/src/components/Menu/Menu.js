a0a99e9bf0d69ca69bdb6a4aecac1699f87e1f30
denis713m <57364006+denis713m@users.noreply.github.com>
2020-11-23 00:48:06 +0200

    create menu with LogOut

---
+++ frontend/src/components/Menu/Menu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Menu.module.sass';
+import {connect} from 'react-redux';
+import {logOut} from '../../redux/actions';
+
+const Menu = (props) => {
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.menuHeader}>
+                <div className={styles.menuTitle}>Menu</div>
+                <div className={styles.btnClose} onClick={props.close}> X </div>
+            </div>           
+            <hr/>
+            <div className={styles.menuItem} onClick={props.logOut}>
+                Log out
+                </div> 
+       
+        </div>
+    );
+}
+
+const mapDispatchToProps = dispatch => ({
+    logOut: () => dispatch(logOut())
+  })
+
+export default connect(null, mapDispatchToProps)(Menu);

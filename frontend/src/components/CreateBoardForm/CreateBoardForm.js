b71383366a77b423700e983cf9400a13be082abc
denis713m <57364006+denis713m@users.noreply.github.com>
2020-11-23 00:52:13 +0200

    create board creation form

---
+++ frontend/src/components/CreateBoardForm/CreateBoardForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import FormInput from '../FormInput/FormInput';
+import customValidator from '../../validators/validator';
+import Schems from '../../validators/YupValidator';
+import styles from './CreateBoardForm.module.sass';
+
+const CreateBoardForm = (props) => {
+    console.log(props);
+    return (
+        <div className={styles.overlay}>
+            <form onSubmit={props.handleSubmit} className={styles.container}>
+                <div className={styles.header}>
+                    <h3 className={styles.title}>Board creation</h3>
+                    <div type='button' className={styles.btnClose} onClick={props.close}>X</div>
+                </div>
+                <Field
+                    name='name'
+                    component={FormInput}
+                    type='text'
+                    label='Board name'
+                />
+                <button type='submit' className={styles.submitContainer}>
+                    <span className={styles.submitBtn}>Create board</span>
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default reduxForm({
+    form: 'createBoard',
+    validate: customValidator(Schems.CreateBoardSchem)
+})(CreateBoardForm);

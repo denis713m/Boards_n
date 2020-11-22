b0b8ab9a3b57f5e889441b292999dd604a8cb267
denis713m <57364006+denis713m@users.noreply.github.com>
2020-11-23 00:24:35 +0200

    Create userSaga

---
+++ frontend/src/redux/saga/userSaga.js
@@ -0,0 +1,48 @@
+import * as types from '../actionTypes';
+import { put } from 'redux-saga/effects';
+import {registerRequest, loginRequest, getUserRequest} from '../../api/fetchApi';
+
+
+export function* registrationUser(action) {
+    yield put({type: types.REGISTRATION_USER_REQUEST});
+    try{
+        const {data} = yield  registerRequest(action.data.values);
+        action.data.history.replace('/');
+        yield put({type: types.REGISTRATION_USER_SUCCESS, data:{
+            firstName: action.data.values.firstName,
+            userId: data.id,
+            lastName: action.data.values.lastName,
+            displayName: action.data.values.displayName,
+        }});
+    }
+    catch (e) {
+        yield put({type: types.REGISTRATION_USER_ERROR, error: e.response});
+    }
+ };
+
+ export function* login(action) {
+    yield put({type: types.REGISTRATION_USER_REQUEST});
+    try{
+        const {data} = yield  loginRequest(action.data.values);
+        console.log(action);
+        action.data.history.replace('/');
+        yield put({type: types.REGISTRATION_USER_SUCCESS, data:data});
+    }
+    catch (e) {
+        console.log(e);
+        yield put({type: types.REGISTRATION_USER_ERROR, error: e.response});
+    }
+ }
+
+ export function* getUser(action) {
+    yield put({type: types.REGISTRATION_USER_REQUEST});
+    try{
+        console.log('getUser');
+        const {data} = yield  getUserRequest();
+        yield put({type: types.REGISTRATION_USER_SUCCESS, data:data});
+    }
+    catch (e) {
+
+        yield put({type: types.REGISTRATION_USER_ERROR, error: e.response});
+    }
+ }

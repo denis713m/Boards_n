87cae32b2e64645437f473d8836ba737cc2dfcaf
denis713m <57364006+denis713m@users.noreply.github.com>
2020-11-23 00:55:55 +0200

    create board page

---
+++ frontend/src/components/CardsList/CardsList.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import RenameListForm from '../RenameListForm/RenameListForm';
+import styles from './CardsList.module.sass';
+
+
+const CardsList = () => {
+    const [isRenameList, changeRenameList]= useState(false);
+    const renameList = async (values)=>{
+        console.log(values);
+        changeRenameList(false)
+    }
+    return (
+        <div className={styles.listContainer}>
+        {!isRenameList && <div className={styles.listTitle} onDoubleClick={() => changeRenameList(true)}>In progress</div>}                             
+                {isRenameList && 
+                        <RenameListForm close={() => changeRenameList(false)} 
+                                                onSubmit={renameList} name={'In progress'}/>}
+
+        <div className={styles.cardContainer}>
+            <div className={styles.card}>In progress</div>
+            <div className={styles.card}>In progress</div>
+            <div className={styles.card}>In progress</div>
+        </div>
+        <button className={styles.btnAddCard}>Add a card...</button>
+    </div>
+    );
+}
+
+export default CardsList;
---
+++ frontend/src/pages/BoardPage/BoardPage.js
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from 'react';
+import styles from './BoardPage.module.sass';
+import {connect} from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import fp from 'lodash/fp';
+import Header from '../../components/Header/Header';
+import {getBoardById, boardRename} from '../../redux/actions';
+import RenameBoardForm from '../../components/RenameBoardForm/RenameBoardForm';
+import Spinner from '../../components/Spinner/Spinner';
+import CardsList from '../../components/CardsList/CardsList';
+
+const BoardPage = (props) => {
+    useEffect(() => {
+        const params = props.location.pathname.substring(7);
+        props.getBoardById(params);
+      }, []);
+    const [isRenameBoard, changeRenameBoard]= useState(false);
+
+    const changeBoardName = (values)=>{
+        props.renameBoard({
+            ...values,
+            id: props.board.id
+        })
+        changeRenameBoard(false)
+    }
+    return (
+        <>
+            <Header/>
+            <div className={styles.container}>
+            {
+                props.isFetching ?
+
+                    <Spinner/>
+
+                :
+                <>
+                    <div className={styles.containerHeader}>
+                    {!isRenameBoard && <span onDoubleClick={() => changeRenameBoard(true)}>{props.board.name}</span>}                             
+                    {isRenameBoard && 
+                            <RenameBoardForm close={() => changeRenameBoard(false)} 
+                                                    onSubmit={changeBoardName} name={props.board.name}/>}
+                        <div>
+                            <span></span>
+                            <span className={styles.dots}>...</span>
+                            <span className={styles.menu}>Show menu</span>
+                        </div>
+                    </div>
+                    <div className={styles.listsContainer}>
+                        <CardsList/>
+                        <CardsList/>
+                    </div>
+                </>
+            }
+
+
+            </div>
+        </>
+    );
+}
+
+const mapStateToProps = (state) => {
+    return {user: state.user.user,
+            board: state.boards.currentBoard,
+            isFetching: state.boards.isFetching
+        };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getBoardById: (data) => dispatch(getBoardById(data)),
+        renameBoard: (data) => dispatch(boardRename(data))
+    }
+};
+
+export default fp.flow(withRouter,connect(mapStateToProps, mapDispatchToProps))(BoardPage);

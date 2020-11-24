1c1c0df1d22596d8a1c10252cf37bfc178f05007
denis713m <57364006+denis713m@users.noreply.github.com>
2020-11-24 22:32:23 +0200

    get rid of 'catch(e){next(e)}'

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -4,58 +4,41 @@ const passwordMiddleware = require('../middleware/password');
 const userQuerie = require('../queries/usersQueries');
 const jwt = require('jsonwebtoken');
 
-module.exports.registrationUser = async (req, res, next) =>{
-    try{
-        const user = req.body;
-        user.password = req.hashPass;
-        tokens = generateTokens.generateTokens(user);
-        const newUser = await userQuerie.createUser({...user, ...tokens});
-        res.send({...tokens, id:newUser.id});
-    }
-    catch (e){
-        next(e);
-    }
-    
+module.exports.registrationUser = async (req, res) =>{
+    const user = req.body;
+    user.password = req.hashPass;
+    tokens = generateTokens.generateTokens(user);
+    const newUser = await userQuerie.createUser({...user, ...tokens});
+    res.send({...tokens, id:newUser.id});    
 };
 
-module.exports.loginUser = async (req, res, next) =>{
-    try{
-        const user = await userQuerie.loginUser(req.body.email);        
-        await passwordMiddleware.passwordCompare(req.body.password, user.password);
-        tokens = generateTokens.generateTokens(user);
-        await userQuerie.updateUserTokens(tokens, user.id)
-        res.send({
-            firstName: user.firstName,
-            userId: user.id,
-            lastName: user.lastName,
-            displayName: user.displayName,
-            accessToken: tokens.accessToken
-        });
-    }
-    catch (e){
-        next(e);
-    }
-    
+module.exports.loginUser = async (req, res) =>{
+    const user = await userQuerie.loginUser(req.body.email);        
+    await passwordMiddleware.passwordCompare(req.body.password, user.password);
+    tokens = generateTokens.generateTokens(user);
+    await userQuerie.updateUserTokens(tokens, user.id)
+    res.send({
+        firstName: user.firstName,
+        userId: user.id,
+        lastName: user.lastName,
+        displayName: user.displayName,
+        accessToken: tokens.accessToken
+    }); 
 };
 
-module.exports.getUser = async (req, res, next) =>{
+module.exports.getUser = async (req, res) =>{
     const accessToken = req.headers.authorization;
     if ( !accessToken) {
-      next(new Error('need token'));
+      throw new Error('need token');
     }
-    try {
-      const tokenData = jwt.verify(accessToken, 'SECRET_FOR_TOKEN');
-      const userId = tokenData.userId;
-      const user = await userQuerie.getUser(userId);
-      res.send({
+    const tokenData = jwt.verify(accessToken, 'SECRET_FOR_TOKEN');
+    const userId = tokenData.userId;
+    const user = await userQuerie.getUser(userId);
+    res.send({
         firstName: user.firstName,
         userId: user.id,
         lastName: user.lastName,
         displayName: user.displayName,
         accessToken: tokens.accessToken
     });
-    } catch (err) {
-      next(err);
-    }
-    
 };
--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const passwordMiddleware = require ('../middleware/password');
+const asyncHandler = require('express-async-handler')
 
 
 router.use('/registration', 
   passwordMiddleware.hashPass,
-  userController.registrationUser);
+  asyncHandler(userController.registrationUser));
   
 router.use('/login', 
-  userController.loginUser);
+  asyncHandler(userController.loginUser));
 
 router.use('/getUser', 
-  userController.getUser);
+  asyncHandler(userController.getUser));
 
 module.exports = router;

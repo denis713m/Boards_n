cacfd5a44431d3f26417d6f3c06a022c99340d8a
denis713m <57364006+denis713m@users.noreply.github.com>
2020-11-28 21:02:57 +0200

    create CardWindow

--- frontend/src/components/BoardMenu/BoardMenu.js
+++ frontend/src/components/BoardMenu/BoardMenu.js
@@ -1,40 +1,40 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './BoardMenu.module.sass';
 
 const BoardMenu = (props) => {
-
     return (
         <div className={styles.mainContainer}>
             <div className={styles.menuHeader}>
                 <div className={styles.menuTitle}>Menu</div>
-                <div className={styles.btnClose} onClick={props.close}> X </div>
+                <div className={styles.btnClose} onClick={props.close}>
+                    X
+                </div>
             </div>
             <hr />
-            <div className={styles.menuItem} 
-            onClick={props.deleteBoard}>Remove board </div>
+            <div className={styles.menuItem} onClick={props.deleteBoard}>
+                Remove board
+            </div>
             <hr />
-            <div className={styles.activity}> 
+            <div className={styles.activity}>
                 <img src={'/activity.jpg'} className={styles.icon}></img>
                 <p>Activity</p>
             </div>
             <div className={styles.activityItem}>
-                <div className={styles.badge}>
-                    T
-                </div>
+                <div className={styles.badge}>T</div>
                 <div className={styles.description}>
                     <span className={styles.author}>test@freshcodeit.com</span>
                     <span className={styles.action}> added </span>
-                    <Link className={styles.card} to={'#'}> Card5</Link>
+                    <Link className={styles.card} to={'#'}>
+                        {' '}
+                        Card5
+                    </Link>
                     <span className={styles.action}> to Queue </span>
-                    <div className={styles.time}>
-                        5 hours ago
-                    </div>
+                    <div className={styles.time}>5 hours ago</div>
                 </div>
             </div>
         </div>
     );
-}
-
+};
 
 export default BoardMenu;

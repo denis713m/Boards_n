645d2237494520fd5ba89001254eab135f05d51f
denis713m <57364006+denis713m@users.noreply.github.com>
2020-12-05 17:28:16 +0200

    add TryAgain component to reload data, make prettier view BoardMenu and userReducer.js

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -3,41 +3,37 @@ import * as types from '../actionTypes';
 const initialState = {
     isFetching: true,
     error: false,
-    user: null,    
-  };
+    user: null,
+};
 
-  export default function(state = initialState, action) {
+export default function (state = initialState, action) {
     switch (action.type) {
-      case types.REGISTRATION_USER_REQUEST:{        
-        return {...state,
-          isFetching: true}
-      }
-      case types.REGISTRATION_USER_SUCCESS:{
-        return {...state,
-            isFetching: false,
-            user: action.data,
-            error: null
+        case types.REGISTRATION_USER_REQUEST: {
+            return { ...state, isFetching: true, error: false };
         }
-      }
-      case types.REGISTRATION_USER_ERROR:{
-        return {
-          ...state,
-          isFetching: false,
-          error: action.error,
-          user: null
+        case types.REGISTRATION_USER_SUCCESS: {
+            state.user = action.data;
+            state.isFetching = false;
+            return { ...state, isFetching: false, user: action.data, error: null };
         }
-      }
-      case types.LOG_OUT:{
-        window.localStorage.removeItem('accessToken');
-        return {
-          ...state,
-          isFetching: false,
-          error: true,
-          user: null
+        case types.REGISTRATION_USER_ERROR: {
+            return {
+                ...state,
+                isFetching: false,
+                error: action.error,
+                user: null,
+            };
         }
-      }
-      default:
-        return state;
+        case types.LOG_OUT: {
+            window.localStorage.removeItem('accessToken');
+            return {
+                ...state,
+                isFetching: false,
+                error: true,
+                user: null,
+            };
+        }
+        default:
+            return state;
     }
-  }
-  
+}

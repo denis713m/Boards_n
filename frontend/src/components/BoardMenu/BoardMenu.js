6a79f41159d2e8fa384996d002c61f01018ac0d3
denis713m <57364006+denis713m@users.noreply.github.com>
2020-12-10 10:57:14 +0200

    create ERROR_CONSTANT to localisation error messages, modify RequireAuth from class to function, beautify RenameListForm and RenameBoardForm

--- frontend/src/components/BoardMenu/BoardMenu.js
+++ frontend/src/components/BoardMenu/BoardMenu.js
@@ -4,14 +4,8 @@ import styles from './BoardMenu.module.sass';
 import Activity from '../Activity/Activity';
 
 const BoardMenu = (props) => {
-    const getActivities = () => {
-        const activities = [];
-        props.activities.forEach((item) =>
-            activities.push(
-                <Activity item={item} key={item.id} card={styles.card}/>
-            )
-        );
-        return activities;
+    const getActivities = () => {        
+        return props.activities.map((item) => <Activity item={item} key={item.id} card={styles.card} />);
     };
     return (
         <div className={styles.mainContainer}>
@@ -22,10 +16,14 @@ const BoardMenu = (props) => {
                 </div>
             </div>
             <hr />
-            <div className={styles.menuItem} onClick={props.deleteBoard}>
-                Remove board
-            </div>
-            <hr />
+            {props.canRemove && (
+                <>
+                    <div className={styles.menuItem} onClick={props.deleteBoard}>
+                        Remove board
+                    </div>
+                    <hr />
+                </>
+            )}
             <div className={styles.activity}>
                 <img src={'/activity.jpg'} className={styles.icon}></img>
                 <p>Activity</p>
